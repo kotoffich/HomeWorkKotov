6e1c0264115bbecc5eeb684cf828523b53df9784
kotoffich <kotoffich@gmail.com>
2017-01-22 14:36:02 +0200

    test git

---
+++ src/_week1/_day2/_Task2.java
@@ -0,0 +1,13 @@
+package _week1._day2;
+
+/**
+ * Created by kotoffich on 22.01.2017.
+ */
+public class _Task2 {
+    public static void main(String[] args) {
+        System.out.println("1.Shop");
+        System.out.println("2.Fresh");
+        System.out.println("3.Bottle");
+        System.out.println("4.Country");
+    }
+}
